d81603ddb08c7c6c1d879960b4ebc74d0e186969
Francesco <cesaranofrancescomain@gmail.com>
2022-11-14 16:33:32 +0100

    stampa nel doom, problema con inner html

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
 // Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
 
-// array lista spesa
-const array = ["patate", "zucchine", "biscotti", "scottex", "detersivo"];
+// dichiarazione variabili 
+    // array lista spesa
+const array = [];
+
+const btn = document.getElementById("btn");
 
 // ciclo per passare in rassegna gli elementi della lista 
 // for (let index = 0; index < array.length; index++) {
@@ -9,9 +12,32 @@ const array = ["patate", "zucchine", "biscotti", "scottex", "detersivo"];
     
 // }
 
-// ciclo while idem 
-let index = 0;
-while (index < array.length) {
+
+
+
+btn.addEventListener ("click",
+function () {
+    const lista = document.getElementById("lista");
+    array.push (lista.value);
+    
+
+    // ciclo while per passare in rassegna gli elementi della lista
+    let index = 0;
+    while (index < array.length) {
+    // devo insrire gli elementi nel doom 
+    // richiamiamo il conainer dove inserire gli elementi 
     console.log(array[index]);
+    let ul = document.getElementById ("ul");
+    // creiamo il list iteam 
+    let li = document.createElement ("li");
+    li.innerHTML =( array[index]);
+
+    // inseriamo li in ul 
+    ul.innerHTML = (li);
+
+    // fine ciclo    
     index++;
-}
+    }
+    
+
+})
