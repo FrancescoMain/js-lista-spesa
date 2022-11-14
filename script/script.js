233a3f8231c9c3d82f06f086b5b558e7c17a75f6
Francesco <cesaranofrancescomain@gmail.com>
2022-11-14 14:44:24 +0100

    lista con ciclo for

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+
+// array lista spesa
+const array = ["patate", "zucchine", "biscotti", "scottex", "detersivo"];
+
+// ciclo per passare in rassegna gli elementi della lista 
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    
+}
